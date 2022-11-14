b4faace855d232e47264eeb418e01a43017157c6
gujianghe <gujianghe@foxmail.com>
2022-11-14 21:33:43 +0800

    Initial commit

---
+++ src/main/java/com/grt/digiOps/DigiOpsApplication.java
@@ -0,0 +1,13 @@
+package com.grt.digiOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grt/digiOps/DigiOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grt.digiOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
